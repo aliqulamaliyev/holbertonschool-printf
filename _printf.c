4130d4061d4a49efcbcc5f2835ffbc1b80a810b3
aliqulamaliyev <aliqulamaliyev@gmail.com>
2025-07-20 07:03:04 +0000

    completes all c tasks

--- _printf.c
+++ _printf.c
@@ -44,3 +44,54 @@ int _printf(const char *format, ...)
 	va_end(args);
 	return (count);
 }
+
+int print_char(va_list args)
+{
+	char c = va_arg(args, int);
+
+	return (write(1, &c, 1));
+}
+
+int print_string(va_list args)
+{
+	char *s = va_arg(args, char *);
+	int len = 0;
+
+	if (!s)
+		s = "(null)";
+	while (s[len])
+		write(1, &s[len++], 1);
+	return (len);
+}
+
+int print_percent(void)
+{
+	return (write(1, "%", 1));
+}
+
+int print_integer(va_list args)
+{
+	int n = va_arg(args, int);
+	char buf[12];
+	int i = 0, count = 0;
+	unsigned int num;
+
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		count++;
+		num = -n;
+	}
+	else
+		num = n;
+
+	do {
+		buf[i++] = (num % 10) + '0';
+		num /= 10;
+	} while (num);
+
+	while (i--)
+		count += write(1, &buf[i], 1);
+
+	return (count);
+}
